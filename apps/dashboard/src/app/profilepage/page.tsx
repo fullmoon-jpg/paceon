b3931dada66cae30ac050b9da9d4e978017655c4
RungkadS <aripurnama193@gmail.com>
2025-09-23 20:33:45 +0700

    perbaikan settings and profile v2

--- apps/dashboard/src/app/profilepage/page.tsx
+++ apps/dashboard/src/app/profilepage/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function ProfilePage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <h1 className="text-3xl font-bold text-gray-700">Coming Soon 🚧</h1>
+        <h1 className="text-3xl font-bold text-gray-700">Coming Soon</h1>
         </div>
     );
 }
