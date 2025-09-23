005fab3ffc8fc8d5dfb6b01be3ada0bad705c681
RungkadS <aripurnama193@gmail.com>
2025-09-23 20:14:12 +0700

    perbaikan settings and profile

--- apps/dashboard/src/app/profilepage/page.tsx
+++ apps/dashboard/src/app/profilepage/page.tsx
@@ -2,14 +2,10 @@
 
 import React from "react";
 
-interface PageTransitionProps {
-  children?: React.ReactNode;
-}
-
-export default function PageTransition({ children }: PageTransitionProps) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-700">Coming Soon 🚧</h1>
-    </div>
-  );
+export default function ProfilePage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-gray-700">Coming Soon 🚧</h1>
+        </div>
+    );
 }
